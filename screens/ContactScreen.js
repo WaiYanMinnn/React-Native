0fded45adc9e7c61385abcd7cbcb2767492b5630
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-04-15 16:35:11 -0700

    adding comments and favorite icons

---
+++ screens/ContactScreen.js
@@ -0,0 +1,27 @@
+import { ScrollView } from "react-native-gesture-handler";
+import { Card,Text } from "react-native-elements";
+
+
+const ContactScreen =()=>{
+    return (
+        <ScrollView>
+            <Card
+                wrapperStyle={{margin:10}}
+            >
+                <Card.Title>
+                    Contact information
+                </Card.Title>
+                <Card.Divider/>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom:10}}>U.S.A.</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text style={{marginBottom:10}}>Email: campsites@nucamp.co</Text>
+
+
+            </Card>
+        </ScrollView>
+    )
+
+}
+export default ContactScreen;
