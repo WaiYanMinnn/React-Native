8df01cb0f04079f5a050c19d22ca48c57a0fec9b
Wai Yan Min <waiyanmin.lane@gmail.com>
2024-04-29 18:37:55 -0700

    rubber band is added to the animatable

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,16 +1,25 @@
+import {useRef} from 'react';
 import { StyleSheet,Text,View,Alert, PanResponder } from 'react-native';
 import {Card,Icon} from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl'; 
 import * as Animatable from 'react-native-animatable';
 
 
-const RenderCampsite=(props)=>{
+const RenderCampsite=(props)=>{      
     const {campsite}=props;
+    const view=useRef();
+
 
     const isLeftSwipe=({dx})=>dx < -200;
 
     const panResponder = PanResponder.create ({
         onStartShouldSetPanResponder:()=>true,
+        onPanResponderGrant:()=>{
+            view.current
+                .rubberBand(1000)
+                .then((endState)=>console.log(endState.finished? 'finished':'canceled'));
+        },
+
         onPanResponderEnd:(e,gestureState)=>{
             console.log('pan responder end ',gestureState);
             if(isLeftSwipe(gestureState)){
@@ -45,7 +54,9 @@ const RenderCampsite=(props)=>{
             animation='fadeInDownBig'
             duration={2000}
             delay={1000}
+            ref={view}
             {...panResponder.panHandlers}
+
             >
 
                 <Card containerStyle={styles.cardContainer}>
